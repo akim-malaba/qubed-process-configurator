8dbd4687ca017a4249a5cfc95adc5acef6e21bf6
Victor Sithole <victorsithole0@gmail.com>
2020-08-05 09:34:36 +0200

    demo

---
+++ src/main/java/com/kafka/api/SpringBootKafkaApiApplication.java
@@ -0,0 +1,19 @@
+package com.kafka.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+
+@EnableSwagger2
+
+@SpringBootApplication
+public class SpringBootKafkaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKafkaApiApplication.class, args);
+	}
+
+}
